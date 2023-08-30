a64212f756c03aa9b97e1a223c9d61d1da65131d
Shade2012 <ubasanino@gmail.com>
2023-08-30 07:43:11 +0700

    style:[S2] add button dan fitur detail

--- lib/data_movie.dart
+++ lib/data_movie.dart
@@ -5,12 +5,14 @@ late String nama;
 late String tahunrilis;
 late String rate;
 late String poster;
+late String desc;
 
-Movie(String nama, String poster,String tahunrilis,String rate,){
+Movie(String nama, String poster,String tahunrilis,String rate,String desc){
   this.nama = nama;
   this.poster = poster;
   this.tahunrilis = tahunrilis;
   this.rate = rate;
+  this.desc = desc;
 
 }
 }
--- lib/listmovie.dart
+++ lib/listmovie.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'data_movie.dart';
+import 'detail_movie.dart';
 class listmovie extends StatefulWidget {
   const listmovie({Key? key}) : super(key: key);
 
@@ -15,10 +16,10 @@ class _listmovieState extends State<listmovie> {
 void initState() {
     // TODO: implement initState
     super.initState();
-    Movie spiritedaway = new Movie("Spirited Away",'asset/spirited_away.jpg', "2001", "8.6" );
-    Movie pussinboots = new Movie("Puss in Boots: The Last Wish",'asset/puss.jpg', "2022", "9.1" );
-    Movie unberabletalent = new Movie("The Unbearable Weight of Massive Talent", 'asset/theunbearable.jpg', "2022", "7.6");
-    Movie riseofguardian = new Movie("Rise of the Guardians", 'asset/rise_of_guardian.jpg', "2012", "7.2");
+    Movie spiritedaway = new Movie("Spirited Away",'asset/spirited_away.jpg', "2001", "8.6","Spirited Away menceritakan tentang anak kecil bernama Chihiro yang terpaksa kerja di dunia makhluk gaib untuk menyelamatkan orangtuanya yang berubah jadi babi gara-gara makan di warung buat makhluk gaib di tempat tersebut." );
+    Movie pussinboots = new Movie("Puss in Boots: The Last Wish",'asset/puss.jpg', "2022", "9.1","Puss in Boots yang berani menemukan bahwa hasratnya akan bahaya dan ketidakpedulian terhadap keselamatan telah memakan korban. Puss telah menghabiskan delapan dari sembilan nyawanya, meskipun ia kehilangan hitungan di sepanjang jalan. Mendapatkan kembali kehidupan itu akan mengirim Puss in Boots pada pencarian termegahnya." );
+    Movie unberabletalent = new Movie("The Unbearable Weight of Massive Talent", 'asset/theunbearable.jpg', "2022", "7.6","Nicolas Cage yang kekurangan uang, menerima tawaran untuk tampil dengan bayaran di pesta ulang tahun miliarder fans sejati, Javi. Hal berubah menjadi sangat tak terduga ketika Cage direkrut oleh seorang agen CIA dan dipaksa untuk hidup bersama dengan Javi sekaligus menjadi informan mereka");
+    Movie riseofguardian = new Movie("Rise of the Guardians", 'asset/rise_of_guardian.jpg', "2012", "7.2","Saat roh jahat dikenal dengan Pitch the Bogeyman mencoba menguasai dunia dengan menciptakan rasa takut pada anak-anak, para Penjaga abadi, yang dikenal sebagai pahlawan dari berbagai kisah anak-anak: Jack Frost, Easter Bunny, Tooth Fairy, Sandman and Santa Claus - harus bersatu untuk pertama kalinya demi melindungi harapan, kepercayaan dan imajinasi anak-anak diseluruh dunia.");
     datamovie.add(riseofguardian);
     datamovie.add(unberabletalent);
     datamovie.add(spiritedaway);
@@ -67,6 +68,27 @@ appBar: AppBar(title: Text("List Film Favorit"),
 
                     Text("Tahun Rilis " + datamovie[index].tahunrilis,
                         style: TextStyle(fontWeight: FontWeight.bold)),
+                    Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 0,top: 15, right: 0,bottom: 5),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailMovie(movie:datamovie[index]),
+                              ),
+                            );
+                          },style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.white),
+                          foregroundColor: MaterialStateProperty.all(Colors.grey),
+                        ),
+                          child: Text('Detail',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),),
+                        ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
 
