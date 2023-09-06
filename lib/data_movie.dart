3451c28a75a666bc21687aa2a061d86c0eea0a15
Shade2012 <ubasanino@gmail.com>
2023-09-06 17:46:46 +0700

    style:[S2] changing the detail page

--- lib/data_movie.dart
+++ lib/data_movie.dart
@@ -6,13 +6,14 @@ late String tahunrilis;
 late String rate;
 late String poster;
 late String desc;
+late String posterls;
 
-Movie(String nama, String poster,String tahunrilis,String rate,String desc){
+Movie(String nama, String poster,String tahunrilis,String rate,String desc,String posterls){
   this.nama = nama;
   this.poster = poster;
   this.tahunrilis = tahunrilis;
   this.rate = rate;
   this.desc = desc;
-
+this.posterls = posterls;
 }
 }
--- lib/detail_movie.dart
+++ lib/detail_movie.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'data_movie.dart';
 
@@ -17,29 +19,71 @@ class _DetailMovieState extends State<DetailMovie> {
       appBar: AppBar(
         title: Text(widget.movie.nama),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(widget.movie.poster),
-            SizedBox(height: 16),
-            Text(
-              "Rating: ${widget.movie.rate}",
-              style: TextStyle(fontWeight: FontWeight.bold),
+      body: Column(
+        children: [
+          // Widget pertama
+          Container(
+            width: 1000, height: 200,
+
+            child: Container(
+              child: Image.asset(widget.movie.posterls,),
+
+
             ),
-            Text(
-              "Tahun Rilis: ${widget.movie.tahunrilis}",
-              style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+
+          // Widget kedua
+          Center(
+
+            child: Container(
+              color: Colors.white,
+
+              margin: EdgeInsets.only(left: 40,right: 40),
+
+                  child: Card(
+                    elevation: 2,
+
+                    color: Colors.white,
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+
+                        children: [
+
+                          Text(
+                            "Rating: ${widget.movie.rate}",
+                            style: TextStyle(fontWeight: FontWeight.bold,),
+                          ),
+                          Text(
+                            "Tahun Rilis: ${widget.movie.tahunrilis}",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "Sinopsis ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            widget.movie.desc ,softWrap: true,style: TextStyle(fontWeight: FontWeight.normal),
+                          )
+
+
+
+
+                          // Add more details about the movie here
+                        ],
+                    ),
+                    ),
+                  )
+
+
+
             ),
-            Text(
-              "Preview : ${widget.movie.desc}",
-              style: TextStyle(fontWeight: FontWeight.normal),
-            ),
-            // Add more details about the movie here
-          ],
-        ),
+          ),
+        ],
+
       ),
+
     );
   }
 }
